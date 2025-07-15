ac0e1a8422c03f5d25a7177053f38fce0694b2c4
ialejandrozalles <zallesrene@gmail.com>
2025-07-14 21:36:40 -0400

    agregando section header

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
 import Header from '../components/Header';
-import Button from '../components/ui/Button.jsx';
+import SectionHeader from '../components/ui/SectionHeader.jsx';
 
 function HomePage() {
   return (
     <div>
       <Header />
+      <SectionHeader title="Direccion Academica" />
+
+      <SectionHeader title="Nuestra Institución" left={true} />
+
+      <SectionHeader title="About Us" left={true} extrabold={true} />
+
+      <SectionHeader title="Services" whiteText={true} />
+
+      <SectionHeader title="Contact" left={true} whiteText={true} />
     </div>
   )
 }
